00587362d0fae881e880d4a142da6689e893e22d
Alex Simuro <aasimuro12@gmail.com>
2019-10-14 21:34:34 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,13 @@
+var env = require('dotenv').config();
+
+exports.spotify = {
+  id: "process.env.03c8f42f0d1b40fe9928ea22a9cba8ef",
+  secret: "process.env.300418cf3bae4208b9ae2c12f997023d"
+};
+
+exports.omdb = {
+  api_key: process.env.OMDB_API_KEY
+};
+
+
+
